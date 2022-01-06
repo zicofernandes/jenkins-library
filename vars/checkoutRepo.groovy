6a50b2f31a7921f8e207d1a7b8444e8fc4e2ed29
zicofernandes <33678232+zicofernandes@users.noreply.github.com>
2022-01-05 22:55:22 -0600

    Update checkoutRepo.groovy

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -1,1 +1,37 @@
+def call(Closure body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  def scmInfo
+  checkoutRepo(config)
+}
 
+def call(Map config) {
+  checkoutRepo(config)
+}
+
+def checkoutRepo(config) {
+  def url_ = config.get('url', "")
+  def branch_ = config.branch
+  def credentialsId_ = config.credentialsId
+ 
+  timestamps {
+    ansiColor('xterm') {
+      if(!url_) {
+        echo "[INFO]: using basic checkout scm since url not defined"
+      }
+
+      if(!branch_) {
+        branch_ = 'master'
+      }
+      deleteDir()
+      if(url_) {
+        git url: url_, branch: branch_, credentialsId: credentialsId_
+      } else {
+        this.scmInfo = checkout scm
+        scmInfo.each{k, v -> env."${k}" = v}
+      }
+    }
+  }
+}
